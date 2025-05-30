915437d67dff54387d29e754a8e0907966bc99fe
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-05-30 10:23:49 +0530

    ctsmn

--- build.sh
+++ build.sh
@@ -11,9 +11,10 @@ cmake --build . --target install -j 4
 cd ..
 cd ..
 rm -rf tdlib
+TDV=$(ls -1 td/lib/libtdjson.so* | tail -n1 | cut -d. -f 2)
 git add -A td
 git config user.name "github-actions[bot]"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git commit -m "Build TDLib" || exit 0
+git commit -m "Build TDLib ${TDV}" || exit 0
 git pull --rebase
 git push
